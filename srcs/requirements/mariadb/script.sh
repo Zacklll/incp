4538fc2040b53b0288a6c60f27b1cedec79ce9a8
Zacklll <zakariawadi16@gmail.com>
2024-11-08 18:45:13 +0000

    new

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,15 @@
+
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+
+mysql -e "FLUSH PRIVILEGES
+
+service mariad stop 
+
+exec mysqld_safe
